491414b34bb0008361ea9e6a7f93d7ee8d2b9f37
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2019-04-22 15:45:42 +0800

    11

--- docker.img.build.sh
+++ docker.img.build.sh
@@ -6,4 +6,6 @@ echo "build aws test app war.."
 
 mvn package
 
-docker build --build-arg AWS_APP_VERSION=$AWS_APP_VERSION  -t aws-test-app1:$AWS_APP_VERSION .
+docker build --build-arg AWS_APP_VERSION=${AWS_APP_VERSION}  -t docker.registry.cscloud.com/aws/test/app1:${AWS_APP_VERSION} .
+
+docker push docker.registry.cscloud.com/aws/test/app1:${AWS_APP_VERSION}
